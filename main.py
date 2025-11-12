1715b42538b49cde9986611523e7bd9d08958018
Bilboow <93973179+Bilboow@users.noreply.github.com>
2025-11-12 23:35:16 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain_groq import ChatGroq
 from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.prompts import PromptTemplate
 
